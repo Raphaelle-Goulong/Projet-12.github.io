24e6c5a48d5a26a1d11390ea518fe8264c69fa41
Raphaelle Goulong <raphaelle.goulong@gmail.com>
2024-05-23 19:54:58 +0200

    feat: component contact link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import '../sass/Contact.scss'
-import backgroundImage from '../assets/images/nikolay-u76CN5rZeOU-unsplash.jpg'
+//import backgroundImage from '../assets/images/nikolay-u76CN5rZeOU-unsplash.jpg'
 
 function Contact() {
     return (
@@ -7,9 +7,28 @@ function Contact() {
             <section className="container_section_contact">
                 <div className="content_section_contact">
                     <h2 className="title_section_contact">Prêt à travailler ensemble ?</h2>
-                     <button className="btn_contact"> Contact Us  </button>              
+                     <button className="btn_contact"> Contact</button>              
                 </div>
-                
+                <div className="contact_file_section">
+                    <h3>Contact</h3>
+                    <div className="contact_social_media">
+                        <span className="link_social">link</span>
+                        <span className="link_social">email</span>
+                        <span className="link_social">X</span>
+
+                    </div>
+                    <form className="form_section_contact" >
+                        {/* <label htmlFor="name">Nom</label> */}
+                        <input type="text" name="name" id="name" placeholder="Nom" required="required" className="input_contact" />
+                        {/* <label htmlFor="firstName">Prénom</label> */}
+                        <input type="text" name="firstName" id="firstName" placeholder="Prénom" required="required" className="input_contact" />
+                        {/* <label htmlFor="email">Email</label> */}
+                        <input type="email" name="email" id="email" placeholder="Email" required="required" className="input_contact"/>
+                        {/* <label htmlFor="message">Message</label> */}
+                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" required="required" className="input_contact"></textarea>
+                        <input type="submit" value="Envoyer" className="input_contact" id="btn_send"/>
+                    </form>
+                </div>
             </section>
         </>
     )
